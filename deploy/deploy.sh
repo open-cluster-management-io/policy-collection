7820b9d30069989b952a9cea3f0ea5c6c3594191
Will <willkutler@gmail.com>
2020-08-03 15:38:02 -0600

    parameterize gh url and namespace in channel path

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-GH_PATH=$1
-NAMESPACE=$2
+GH_URL=$1
+GH_PATH=$2
+NAMESPACE=$3
 
-PATCH_CFG=$(cat "patch_template.json" | sed "s/##GH_PATH##/${1:-stable}/g")
-echo "$PATCH_CFG" > dir_patch.json
-KUST_CFG=$(cat "kustomization_template.yaml" | sed "s/##NAMESPACE##/${2:-policies}/g")
+URL_CFG=$(cat "url_template.json" | sed "s%##GH_URL##%${1:-https://github.com/open-cluster-management/policy-collection.git}%g")
+echo "$URL_CFG" > url_patch.json
+PATH_CFG=$(cat "path_template.json" | sed "s/##GH_PATH##/${2:-stable}/g")
+echo "$PATH_CFG" > dir_patch.json
+CHANNEL_CFG=$(cat "channel_template.json" | sed "s/##NAMESPACE##/${3:-policies}/g")
+echo "$CHANNEL_CFG" > channel_patch.json
+KUST_CFG=$(cat "kustomization_template.yaml" | sed "s/##NAMESPACE##/${3:-policies}/g")
 echo "$KUST_CFG" > kustomization.yaml
 
-kustomize build . > resources.yaml
+kustomize build .
 kubectl apply -f resources.yaml
 
 rm dir_patch.json
+rm url_patch.json
+rm channel_patch.json
 rm kustomization.yaml
 rm resources.yaml
